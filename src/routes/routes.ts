fb16fd54e9e3da35209ee1c3f862cad732242ef7
Uriel R2 <erodriguez@tiprotec.com>
2024-01-29 17:03:10 -0600

    UPDATE: se agrega carga perezosa lazy-load

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,4 +1,5 @@
 import { LazyExoticComponent, lazy } from "react";
+import { NoLazy } from "../01-lazyLoad/pages/NoLazy";
 /* import { LazyPage1, LazyPage2, LazyPage3 } from "../01-lazyLoad/pages"; */
 
 type JSXComponent = () => JSX.Element; // JSXComponent es de tipo "() => JSX.Element" componenete
@@ -11,29 +12,26 @@ interface Route {
 }
 
 //lazy son pedazos de mi app, es decir, componentes que se van a cargar solo cunado sean consumidos, para no construir todo el proyecto si no se va acceder a el
-const Lazy1 = lazy( () => import(/* webpackChunkName: "Lazy1" */  '../01-lazyLoad/pages/LazyPage1'));
-const Lazy2 = lazy( () => import(/* webpackChunkName: "Lazy2" */  '../01-lazyLoad/pages/LazyPage2'));
-const Lazy3 = lazy( () => import(/* webpackChunkName: "Lazy3" */  '../01-lazyLoad/pages/LazyPage3'));
+// const Lazy2 = lazy( () => import(/* webpackChunkName: "Lazy2" */  '../01-lazyLoad/pages/LazyPage2'));
+//const Lazy3 = lazy( () => import(/* webpackChunkName: "Lazy3" */  '../01-lazyLoad/pages/LazyPage3'));
+
+
+const LazyLayout = lazy( () => import(/* webpackChunkName: "Lazy" */  '../01-lazyLoad/layout/LazyLayout'));
+
 
 // 'Route[]' =>  es un arreglo de la interface route 
 export const routes: Route[] = [
     {
-        to:'/lazy1',
-        path:'lazy1',
-        Component: Lazy1,
-        name: 'lazy-1'
+        path:'/lazyload/*', // /lazyload/* la ruta lazy-layout va a tener mas rutas y se van a manejar despues del path /lazyload/*
+        to:'/lazyload/',  
+        Component: LazyLayout,
+        name: 'lazyload'
     },
     {
-        to:'/lazy2',
-        path:'lazy2',
-        Component: Lazy2,
-        name: 'lazy-2'
-    },
-    {
-        to:'/lazy3',
-        path:'lazy3',
-        Component: Lazy3,
-        name: 'lazy-3'
+        to:'/no-lazy',
+        path:'no-lazy',
+        Component: NoLazy,
+        name: 'no-lazy'
     }
 
 ]
